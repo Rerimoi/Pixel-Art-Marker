b62341eac6ec6bfc3a4b7035368ab04128e2478a
Rerimoi <prkwonyike@gmail.com>
2018-07-01 22:36:53 +0300

    fixing bugs

--- designs.js
+++ designs.js
@@ -4,6 +4,10 @@
 // When size is submitted by the user, call makeGrid()
 
 
+
+
+$(document).ready(function()){
+
 var color, rows,columns;
 
 
@@ -12,8 +16,10 @@ $('#sizePicker').submit (function makeGrid(grid) {
   var rows=$('#inputHeight').val();
   var columns=$('#inputWeight').val();
   var color=$('#colorPicker').val();
+  makeGrid(rows, columns);
 
-  $(tr).remove();
+function makeGrid(rows,columns){
+  $('tr').remove();
 
 // for loop to set the  no of rows and columns grid to appear after submit button is clicked
   for (var i = 1; i <= rows; i++) {
@@ -30,6 +36,7 @@ $('#sizePicker').submit (function makeGrid(grid) {
     var color=$('#colorPicker').val();
     $(event.target).css('background-color',color);
   });
+}
 
-
+});
 });
