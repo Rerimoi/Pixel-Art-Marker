a811d381c39c9f008ec95be49849fba2a53cc284
Rerimoi <prkwonyike@gmail.com>
2018-07-01 22:58:04 +0300

    changes onthe makeGrid function and events

--- designs.js
+++ designs.js
@@ -6,36 +6,42 @@
 
 
 
-$(document).ready(function()){
-
 var color, rows,columns;
 
 
-$('#sizePicker').submit (function makeGrid(grid) {
+$('#sizePicker'). on('submit', function (event){
+  event.preventDefault();
    // declare the rows and columns
   var rows=$('#inputHeight').val();
   var columns=$('#inputWeight').val();
   var color=$('#colorPicker').val();
   makeGrid(rows, columns);
+});
 
+function makeGrid(x,y){
 
   $('tr').remove();
 
 // for loop to set the  no of rows and columns grid to appear after submit button is clicked
-  for (var i = 1; i <= rows; i++) {
+  for (var i = 1; i <= x; i++) {
     $('#pixelCanvas').append('<tr></tr>');
 
-    for (var j = 1; i < columns; j++) {
+    for (var j = 1; j <= y; j++) {
       $('#pixelCanvas').filter(':last').append('<td></td>');
 
     }
   }
-  grid.preventDefault();
+
 
   $('td').click(function(event){
     var color=$('#colorPicker').val();
-    $(event.target).css('background-color',color);
+    if ($(this).attr('style')) {
+       $(this).removeAttr('style')
+   }
+    else {
+       $(this).css('background-color',color);
+   }
+
   });
 
-});
-});
+};
