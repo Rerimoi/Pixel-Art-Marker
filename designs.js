6ab308628db853e861543479a889b896aaff92a0
Rerimoi <prkwonyike@gmail.com>
2018-07-01 20:15:26 +0300

    declared the rows and columns and set the for loop

--- designs.js
+++ designs.js
@@ -3,8 +3,20 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+$('#sizePicker').submit (function makeGrid(grid) {
+   // declare the rows and columns
+  var n=$('#inputHeight').value();
+  var m=$('#inputWeight').value();
+// for loop to set the  no of rows and columns grid to appear after submit button is clicked
+  for (var i = 1; i <= n; i++) {
+    $('table').append('<tr></tr>');
 
-}
+    for (var j = 1; i < m; j++) {
+      $('tr:last').append('<td></td>');
+    }
+  }
+
+
+
+})
